935ddaccd2cc1fe4f247bbf5688a6e2f390898ba
koss054 <konstantin054@abv.bg>
2022-10-29 22:46:42 +0300

    Add solution to the third problem for exercise 2

--- javascript_course/js/ex2.js
+++ javascript_course/js/ex2.js
@@ -16,3 +16,23 @@ document.getElementById("fahr_3").innerHTML =
 let classification = [ "John Hill" , "Mary Jane", "Gary Vee", "Paricia Mills", "Helen Hall", "Paul Green" ]; 
 
 document.getElementById("best_students").innerHTML = classification.slice(0, 3);
+
+// Third problem solution
+let course = {
+    'title': "Learn to Code in Python 3",
+    'categories': ['programming', 'technology', 'python'],
+    '5_stars_reviews': 420,
+    '4_stars_reviews': 80,
+    '3_stars_reviews': 33,
+    '2_stars_reviews': 20,
+    '1_stars_reviews': 4
+}
+
+document.getElementById("course_title").innerHTML = course.title;
+document.getElementById("main_category").innerHTML = course.categories[0];
+
+document.getElementById("reviews_5_stars").innerHTML =
+    Math.round(
+    (course["5_stars_reviews"] * 100) /
+    (course["5_stars_reviews"] + course["4_stars_reviews"] + course["3_stars_reviews"] + course["2_stars_reviews"] + course["1_stars_reviews"]))
+    .toString() + "%";
