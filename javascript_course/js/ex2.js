11863243539791370f0aca371ab423073ddb8e99
koss054 <konstantin054@abv.bg>
2022-10-29 22:33:50 +0300

    Add solution to the first problem for exercise 2

--- javascript_course/js/ex2.js
+++ javascript_course/js/ex2.js
@@ -1,2 +1,13 @@
 // First problem solution
+function convertTemperature(celsius) {
+    return (9 * celsius / 5) + 32;
+}
 
+document.getElementById("fahr_1").innerHTML =
+    convertTemperature(parseInt(document.getElementById("celsius_1").innerHTML));
+
+document.getElementById("fahr_2").innerHTML =
+    convertTemperature(parseInt(document.getElementById("celsius_2").innerHTML));
+
+document.getElementById("fahr_3").innerHTML =
+    convertTemperature(parseInt(document.getElementById("celsius_3").innerHTML));
