b7adc79cab320bdb2b0edb4aded9584c4e3715fe
koss054 <konstantin054@abv.bg>
2022-10-29 21:21:47 +0300

    Add solution to the second problem for exercise 1

--- javascript_course/js/ex1.js
+++ javascript_course/js/ex1.js
@@ -1,1 +1,11 @@
 console.log("This is ex1.js");
+
+let firstName = "Koss";
+let lastName = "Kosinov";
+let yearOfBirth = 2000;
+let element = document.getElementById("student_message");
+
+let textForPage = "Hi, my name is " + firstName + " " + lastName + ", I'm " + (2022 - yearOfBirth).toString() + " years old and I'm learning Javascript";
+
+element.textContent = textForPage;
+
