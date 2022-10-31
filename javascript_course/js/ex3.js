9e89ba414f36bdff345d82aad8a00489af803cac
koss054 <konstantin054@abv.bg>
2022-10-31 11:50:11 +0200

    Add solution to the second problem for exercise 3

--- javascript_course/js/ex3.js
+++ javascript_course/js/ex3.js
@@ -7,3 +7,14 @@ document.getElementById("convert").onclick = function() {
     let celsius = document.getElementById("temp_celsius").value;
     document.getElementById("temp_fahr").textContent = convertTemperature(celsius);
 };
+
+// Second problem solution
+let startYear = 2022;
+let endYear = 2050;
+let yearElement = document.getElementById("anos_copa");
+
+yearElement.textContent = "";
+
+for (; startYear <= endYear; startYear += 4) {
+    yearElement.innerHTML += '<li>' + startYear + '</li>';
+}
