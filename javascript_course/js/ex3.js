dc0df483c34448ecfed4b0368053da6dcccb5caf
koss054 <konstantin054@abv.bg>
2022-10-31 12:19:55 +0200

    Add solution to the third problem for exercise 3

--- javascript_course/js/ex3.js
+++ javascript_course/js/ex3.js
@@ -18,3 +18,38 @@ yearElement.textContent = "";
 for (; startYear <= endYear; startYear += 4) {
     yearElement.innerHTML += '<li>' + startYear + '</li>';
 }
+
+// Third problem solution
+document.getElementById("calculate").onclick = function() {
+    let studentResultElement = document.getElementById("result");
+
+    let firstGrade = parseInt(document.getElementById("grade1").value);
+    let secondGrade =parseInt(document.getElementById("grade2").value);
+    let absences = document.getElementById("absences").value;
+
+    console.log(firstGrade);
+    console.log(secondGrade);
+    console.log(absences);
+
+    const TOTAL_CLASSES = 20;
+    const MIN_ABSENCE_ALLOWED = 30;
+    const MIN_GRADE_REQUIRED = 6.5;
+
+    let absencesPercentage = (absences * 100) / TOTAL_CLASSES;
+    let averageGrade = (firstGrade + secondGrade) / 2;
+
+    console.log(absencesPercentage);
+    console.log(averageGrade);
+
+    studentResultElement.textContent = "Grade: " + averageGrade + " || Missed Classes: " + absencesPercentage + "% || ";
+
+    if (absencesPercentage > MIN_ABSENCE_ALLOWED && averageGrade < 6.5) {
+        studentResultElement.textContent += "FAILED BECAUSE OF: Both";
+    } else if (absencesPercentage > MIN_ABSENCE_ALLOWED) {
+        studentResultElement.textContent += "FAILED BECAUSE OF: Absences";
+    } else if (averageGrade < MIN_GRADE_REQUIRED) {
+        studentResultElement.textContent += "FAILED BECAUSE OF: Insufficient Grade";
+    } else {
+        studentResultElement.textContent += "PASSED";
+    }
+}
