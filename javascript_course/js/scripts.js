13c7f4df36ab25cbfb34d8eda7848caab46fcdf0
koss054 <konstantin054@abv.bg>
2022-10-31 16:16:14 +0200

    Solve Date & Time extra challenge

--- javascript_course/js/scripts.js
+++ javascript_course/js/scripts.js
@@ -2,9 +2,18 @@
 console.log("Testing the console log"); */
 
 // Date & Time Challenge
-let orderDate = new Date("2020-03-26");
+/* let orderDate = new Date("2020-03-26");
 let deliverDate = new Date("2020-04-02");
 
 let difference = Math.floor((deliverDate - orderDate) / 86400000);
 
-document.getElementById("delivery_time").textContent = difference.toString();
+document.getElementById("delivery_time").textContent = difference.toString(); */
+
+// Date & Time Extra CHallenge
+let orderDate = new Date("2020-03-26");
+let daysToDeliver = 7;
+
+let deliverDate = orderDate;
+deliverDate.setDate(orderDate.getDate() + daysToDeliver);
+
+console.log(deliverDate);
