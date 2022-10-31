25a0ec5dec36725da65ed458e6a44da43c17a649
koss054 <konstantin054@abv.bg>
2022-10-31 16:45:26 +0200

    Solve Clock Challenge from the Time Methods lesson

--- javascript_course/js/scripts.js
+++ javascript_course/js/scripts.js
@@ -10,10 +10,29 @@ let difference = Math.floor((deliverDate - orderDate) / 86400000);
 document.getElementById("delivery_time").textContent = difference.toString(); */
 
 // Date & Time Extra CHallenge
-let orderDate = new Date("2020-03-26");
+/* let orderDate = new Date("2020-03-26");
 let daysToDeliver = 7;
 
 let deliverDate = orderDate;
 deliverDate.setDate(orderDate.getDate() + daysToDeliver);
 
-console.log(deliverDate);
+console.log(deliverDate); */
+
+// Clock Challenge
+let hoursElement = document.getElementById("hours");
+let minutesElement = document.getElementById("minutes");
+let secondsElement = document.getElementById("seconds");
+
+window.setInterval(function(){
+    let hours = new Date().getHours();
+    let minutes = new Date().getMinutes();
+    let seconds = new Date().getSeconds();
+
+    hours = hours < 10 ? "0" + hours.toString() : hours.toString();
+    minutes = minutes < 10 ? "0" + minutes.toString() : minutes.toString();
+    seconds = seconds < 10 ? "0" + seconds.toString() : seconds.toString();
+
+    hoursElement.textContent = hours;
+    minutesElement.textContent = minutes;
+    secondsElement.textContent = seconds;
+}, 1000);
