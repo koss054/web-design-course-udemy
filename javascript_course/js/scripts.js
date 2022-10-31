e0a4896ffe957b349d78c5ba57de38ebff7dea61
koss054 <konstantin054@abv.bg>
2022-10-31 16:04:28 +0200

    Solve Date & Time lesson challenge

--- javascript_course/js/scripts.js
+++ javascript_course/js/scripts.js
@@ -1,2 +1,10 @@
-alert("Testing the alert command");
-console.log("Testing the console log");
+/* alert("Testing the alert command");
+console.log("Testing the console log"); */
+
+// Date & Time Challenge
+let orderDate = new Date("2020-03-26");
+let deliverDate = new Date("2020-04-02");
+
+let difference = Math.floor((deliverDate - orderDate) / 86400000);
+
+document.getElementById("delivery_time").textContent = difference.toString();
