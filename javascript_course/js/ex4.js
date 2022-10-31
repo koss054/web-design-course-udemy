00dd2ab372b562bb9386130b80568fd84759e5f0
koss054 <konstantin054@abv.bg>
2022-10-31 18:40:58 +0200

    Show current date in Order Date field after selection

--- javascript_course/js/ex4.js
+++ javascript_course/js/ex4.js
@@ -1,1 +1,27 @@
 // First problem solution
+let shipments = 
+        {
+            'standard': {
+                'leadTime': 7,
+                'fee': 2
+            },
+            'express': {
+                'leadTime': 3,
+                'fee': 4.5
+            }
+        };
+
+document.getElementById("shipments").onchange = function() {
+    let selectedField = document.getElementById("shipments");
+    let selectedOption = selectedField.options.selectedIndex;
+
+    let shipmentType  = "";
+    shipmentType = selectedOption == 1 ? shipmentType = 'standard' : shipmentType = 'express';
+
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    let currentMonth = date.getMonth() + 1;
+    let currentDay = date.getDate();
+
+    document.getElementById("order_date").textContent = currentYear + "-" + currentMonth + "-" + currentDay;
+}
