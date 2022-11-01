b00a574f410f84a1c1da42c956649edb3ee211ae
koss054 <konstantin054@abv.bg>
2022-11-01 13:51:13 +0200

    Make hamburger icon in mobile view open and hide the sliding side menu

--- Wedesign_en/js/scripts.js
+++ Wedesign_en/js/scripts.js
@@ -1,7 +1,13 @@
 
 // Mobile Menu
 
+document.getElementById("hamburguer-icon").onclick = function() {
+  document.getElementById("sliding-header-menu-outer").style.right = "0";
+}
 
+document.getElementById("sliding-header-menu-close-button").onclick = function() {
+  document.getElementById("sliding-header-menu-outer").style.right = "-320px";
+}
 
 // About us Tab
 
